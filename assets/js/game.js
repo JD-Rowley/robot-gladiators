b09c9eeb72c5307019163a2e2c05b1f06693d2bc
JD Rowley <jdrowley1989@gmail.com>
2021-08-04 17:21:53 -0600

    add high score feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -133,11 +133,19 @@ var startGame = function() {
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
 
-    // If player is still alive, they win
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    // Check localStorage for high score, if it's not there, use 0
+    var highscore = localStorage.getItem("highscore");
+    if (highscore === null) {
+        highscore = 0;
+    }
+    // If player has more money than the high score, player has new high score
+    if (playerInfo.money > highscore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");  
     } else {
-        window.alert("You've lost your robot in battle!");
+        alert(playerInfo.name + "did not beat the high score of " + highscore + ". Maybe next time!");
     }
 
     // Ask player if they'd like to play again
